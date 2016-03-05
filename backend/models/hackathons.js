3a8228770b4ee9dbd9912dbfe9af6b1770708691
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 16:10:42 -0500

    Missed an e

--- backend/models/hackathons.js
+++ backend/models/hackathons.js
@@ -1,5 +1,5 @@
 var database = require("../database");
 
-module.exports.creatHackathons = function(name,shortname,description,url,starts_at,ends_at){
+module.exports.createHackathons = function(name,shortname,description,url,starts_at,ends_at){
 	database.query("INSERT INTO hackathons('name','shortname','description','url','starts_at','ends_at') VALUES("+ database.escape(name)+ ","+ database.escape(shortname)+ ","+ database.escape(description)+ ","+ database.escape(url)+ ","+ database.escape(starts_at)+ ","+ database.escape(ends_at)+ ")", callback);
 };
