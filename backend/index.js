40f4774b9bd12e5b048b7d3181d67e0ef52158f4
Jonathan Warner <jaxbot@gmail.com>
2016-03-05 16:29:31 -0500

    Stub out API example

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ app.get('/', function(req, res){
 	res.send('HackerTown');
 });
 
+// API
+app.get('/api/hackathons/:hackathonId', function(req, res) {
+	console.log(req.params);
+	res.send('k');
+});
+
 app.listen(3000, function(){
 	console.log('HackerTown running at http://localhost:3000/');
 });
