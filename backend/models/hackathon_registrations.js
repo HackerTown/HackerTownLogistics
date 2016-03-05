ac81ce4376ea0001974df9b1d39f83c619191a74
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 16:12:54 -0500

    Added all database fields

--- backend/models/hackathon_registrations.js
+++ backend/models/hackathon_registrations.js
@@ -1,5 +1,5 @@
 var database = require("../database");
 
-module.exports.registerhacker = function(name,shortname,description,url,starts_at,ends_at){
+module.exports.registerhacker = function(hackathon_id,name,school,phone_number,student_email,preffered_email,gender,resume_id,age,ethnicity,location,school_year,major,hackathon_attendance_count){
 	database.query("INSERT INTO hackathons('name','shortname','description','url','starts_at','ends_at') VALUES("+ database.escape(name)+ ","+ database.escape(shortname)+ ","+ database.escape(description)+ ","+ database.escape(url)+ ","+ database.escape(starts_at)+ ","+ database.escape(ends_at)+ ")", callback);
 };
