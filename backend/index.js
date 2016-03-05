32447bebe28f0c74700966e21a77418a5bdbae6a
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 10:59:28 -0500

    Adding the index backend thing

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var database = require('./database');
 
+app.use(express.static('../public'));
+
 app.get('/', function(req, res){
 	res.send('HackerTown');
 });
 
 app.listen(3000, function(){
 	console.log('HackerTown running at http://localhost:3000/');
-})
+});
