b89bfa1b8298d6e9808c8701801bf2337480dd8e
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 16:33:07 -0500

    Added getter function to module

--- backend/models/hackathons.js
+++ backend/models/hackathons.js
@@ -3,3 +3,7 @@ var database = require("../database");
 module.exports.createHackathons = function(name,shortname,description,url,starts_at,ends_at){
 	database.query("INSERT INTO hackathons('name','shortname','description','url','starts_at','ends_at') VALUES("+ database.escape(name)+ ","+ database.escape(shortname)+ ","+ database.escape(description)+ ","+ database.escape(url)+ ","+ database.escape(starts_at)+ ","+ database.escape(ends_at)+ ")", callback);
 };
+
+module.exports.getHackathonById = function(id, callback) {
+	database.query("SELECT * FROM users WHERE id = " + (~~id), callback);
+};
