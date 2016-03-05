c1dc7e5f81310b7f8280eee7b430ea0088564a40
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 11:49:19 -0500

    Adding hackathon admins models

---
+++ backend/models/hackathon_admins.js
@@ -0,0 +1,5 @@
+var database = require("../database");
+
+module.exports.createId = function(user_id, hackathon_id){
+	database.query("INSERT INTO hackathon_admins ('user_id','hackathon_id') VALUES("+ database.escape(user_id)+ ","+ database.escape(hackathon_id)+ ")", callback);
+};
