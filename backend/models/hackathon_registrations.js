12260f256fc2cd6deecd1946e5680002a5d3c65b
Elise Heron <eheron@knights.ucf.edu>
2016-03-05 16:09:42 -0500

    Changed name again

---
+++ backend/models/hackathon_registrations.js
@@ -0,0 +1,5 @@
+var database = require("../database");
+
+module.exports.registerhacker = function(name,shortname,description,url,starts_at,ends_at){
+	database.query("INSERT INTO hackathons('name','shortname','description','url','starts_at','ends_at') VALUES("+ database.escape(name)+ ","+ database.escape(shortname)+ ","+ database.escape(description)+ ","+ database.escape(url)+ ","+ database.escape(starts_at)+ ","+ database.escape(ends_at)+ ")", callback);
+};
