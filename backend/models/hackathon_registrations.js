6bfb8ad1d1709b63dd9af7f6d5752cd00e6af245
Elise Heron <eheron@knights.ucf.edu>
2016-03-09 23:35:40 -0500

    added getter for registrations

--- backend/models/hackathon_registrations.js
+++ backend/models/hackathon_registrations.js
@@ -4,3 +4,7 @@ module.exports.registerhacker = function(hackathon_id, name, school, phone_numbe
 	database.query("INSERT INTO hackathon_registration SET ?", {hackathon_id: hackathon_id, name: name, school: school, phone_number: phone_number, student_email: student_email, preffered_email: preffered_email, gender: gender, resume_id: resume_id, age: age, ethnicity: ethnicity, location: location, school_year: school_year, major: major, hackathon_attendance_count: hackathon_attendance_count}, callback);
 
 };
+
+module.exports.getRegistrationById = function(id, callback){
+	database.query("SELECT * FROM hackathon_registration WHERE id = " + (~~id), callback);
+}
