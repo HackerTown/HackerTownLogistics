ac47a67161efa7eedcedf6d27f45cf793c9999bd
Elise Heron <eheron@knights.ucf.edu>
2016-03-09 23:21:27 -0500

    added getter function to hackathon admins

--- backend/models/hackathon_admins.js
+++ backend/models/hackathon_admins.js
@@ -3,3 +3,7 @@ var database = require("../database");
 module.exports.createHackathonAdmin = function(user_id, hackathon_id){
 	database.query("INSERT INTO hackathon_admins SET ?",{user_id: user_id, hackathon_id: hackathon_id}, callback);
 };
+
+module.exports.getHackathonAdminById = function(id, callback) {
+	database.query("SELECT * FROM hackathon_admins WHERE id = " + (~~id),callback);
+};
