90712285601c7e23c7bfdc93f52fac068aaa9b6b
Elise Heron <eheron@knights.ucf.edu>
2016-03-09 23:12:01 -0500

    rewrote model to match hackathons.js

--- backend/models/hackathon_admins.js
+++ backend/models/hackathon_admins.js
@@ -1,5 +1,5 @@
 var database = require("../database");
 
 module.exports.createHackathonAdmin = function(user_id, hackathon_id){
-	database.query("INSERT INTO hackathon_admins ('user_id','hackathon_id') VALUES("+ database.escape(user_id)+ ","+ database.escape(hackathon_id)+ ")", callback);
+	database.query("INSERT INTO hackathon_admins SET ?",{user_id: user_id, hackathon_id: hackathon_id}, callback);
 };
