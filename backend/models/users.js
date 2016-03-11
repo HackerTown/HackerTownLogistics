5f902d4fe489ce7c1ec1941719aab2afdbfd1c70
Elise Heron <eheron@knights.ucf.edu>
2016-03-11 00:30:59 -0500

    fixed user model to match the others and added the other fields to it

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,7 +1,7 @@
 var database = require("../database");
 
-module.exports.createUser = function(username, email) {
-	database.query("INSERT INTO users ('username', 'email') VALUES (" + database.escape(username) + "," + database.escape(email) + ")", callback);
+module.exports.createUser = function(username, email, name, password, school) {
+	database.query("INSERT INTO users SET ?", {username: username, email: email, name: name:, password: password, school: school}, callback);
 };
 
 module.exports.getUserById = function(id, callback) {
