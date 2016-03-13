a169f638ac1b20019697238b0c96245ec858a8fa
Elise Heron <eheron@knights.ucf.edu>
2016-03-12 22:36:48 -0500

    added babel to the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 
 gulp.task('default', function(){
 	//not sure what goes here or what I am doing
+	return gulp.src("src/app.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
 });
