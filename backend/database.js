e98ca7c618ec029502476e62b5e824f66ed3d8f3
Jonathan Warner <jaxbot@gmail.com>
2016-03-15 23:04:47 -0400

    Add failure and retry features to db connector

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,38 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var config = {
 	host	:'localhost',
 	user	:'root',
 	password : '',
 	database : 'hackertown'
-});
+};
 
-connection.connect();
+var connection;
+function setup() {
+	connection = mysql.createConnection(config);
+	connection.connect();
+	connection.on('error', function(err) {
+        console.log("connection error");
+		console.log(err);
+		setup();
+	});
+}
+setup();
 
-module.exports = connection;
+module.exports.escape = connection.escape;
+module.exports.query = function() {
+  var parameters = Array.prototype.slice.call(arguments);
+  var retry_count = 0;
+  var f = function() {
+    try {
+      connection.query.apply(connection, parameters);
+    } catch (e) {
+      console.log(e);
+      setup();
+      if (retry_count < 5) {
+        setTimeout(function() { f(); }, 100);
+        retry_count++;
+      }
+    }
+  };
+  f();
+}
